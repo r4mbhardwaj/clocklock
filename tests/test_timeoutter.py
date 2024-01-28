70b1be92e0ca99feaeff030f4331968ea29af9ae
Ram <r4mbhardwaj@gmail.com>
2024-01-28 23:07:53 +0530

    Add test for running functions with timeout in multiple threads

--- tests/test_timeoutter.py
+++ tests/test_timeoutter.py
@@ -22,3 +22,46 @@ def test_run_with_timeout_with_fallback():
         fallback=lambda: "Fallback function called",
     )
     assert result == "Fallback function called"
+
+
+import threading
+import time
+from clocklock import timeout_fn
+
+
+def run_with_timeout_and_collect(results, id, seconds=1, fallback=None):
+    def long_running_function():
+        time.sleep(seconds - 0.5)  # Making sure it completes before the timeout
+        return f"Function completed successfully: {id}"
+
+    result = timeout_fn(
+        seconds=seconds,
+        func=long_running_function,
+        fallback=fallback,
+    )
+    results[id] = result
+
+
+def test_run_with_timeout_in_threads():
+    number_of_threads = 10
+    timeout_duration = 1
+    results = {}
+
+    # Start all threads
+    threads = []
+    for i in range(number_of_threads):
+        thread = threading.Thread(
+            target=run_with_timeout_and_collect, args=(results, i, timeout_duration)
+        )
+        threads.append(thread)
+        thread.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+    # Check the results
+    for i in range(number_of_threads):
+        assert (
+            results[i] == f"Function completed successfully: {i}"
+        ), f"Test failed for thread {i}"
