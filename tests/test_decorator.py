fe03870465f3956774cc0a0930b2557f3e44778d
Ram <r4mbhardwaj@gmail.com>
2024-01-28 23:06:38 +0530

    Add test case for threading with timeout decorator

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -36,3 +36,35 @@ def test_timeout_decorator_with_fallback_with_args():
     term = 12
     result = long_running_function(x=term)
     assert result == statement.format(term)
+
+
+import threading
+import queue
+
+
+def test_with_threading():
+    def run_long_running_function(results_queue):
+        result = long_running_function()
+        results_queue.put(result)
+
+    @timeout(seconds=3, fallback=lambda: (False, ["Profanity timeout"]))
+    def long_running_function():
+        time.sleep(2)
+        return "Function completed successfully"
+
+    threads = []
+    results_queue = queue.Queue()
+    for _ in range(10):
+        t = threading.Thread(target=run_long_running_function, args=(results_queue,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    results = []
+    while not results_queue.empty():
+        results.append(results_queue.get())
+
+    print(results)
+    assert all([result == "Function completed successfully" for result in results])
